d798d1fda024377afbea70c79b09d7ef78b7f048
Nicolas Cabezas <nicolas.cabezas@outlook.com>
2025-05-18 11:06:28 -0500

    account

--- ReconocerApp.SPA/src/ui-components/SignOutButton.jsx
+++ ReconocerApp.SPA/src/ui-components/SignOutButton.jsx
@@ -4,12 +4,11 @@ import IconButton from '@mui/material/IconButton';
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
-import { AccountPicker } from "./AccountPicker";
+import Typography from '@mui/material/Typography';
+import { Box, Avatar, Divider } from "@mui/material";
 
 export const SignOutButton = () => {
     const { instance } = useMsal();
-    const [accountSelectorOpen, setOpen] = useState(false);
-
     const [anchorEl, setAnchorEl] = useState(null);
     const open = Boolean(anchorEl);
 
@@ -23,42 +22,48 @@ export const SignOutButton = () => {
         }
     }
 
-    const handleAccountSelection = () => {
-        setAnchorEl(null);
-        setOpen(true);
-    }
-
-    const handleClose = () => {
-        setOpen(false);
-    };
-
     return (
-        <div>
+        <Box>
             <IconButton
                 onClick={(event) => setAnchorEl(event.currentTarget)}
                 color="inherit"
             >
                 <AccountCircle />
             </IconButton>
+
             <Menu
                 id="menu-appbar"
                 anchorEl={anchorEl}
                 anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                    vertical: 'bottom',
+                    horizontal: 'right',
                 }}
                 keepMounted
                 transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                    vertical: 'top',
+                    horizontal: 'right',
                 }}
                 open={open}
                 onClose={() => setAnchorEl(null)}
             >
-                <MenuItem onClick={() => handleAccountSelection()} key="switchAccount">Cambiar de cuenta</MenuItem>
-                <MenuItem onClick={() => handleLogout("popup")} key="logoutPopup">Cerrar sesión</MenuItem>
+                {instance.getActiveAccount() && (
+                    <Box display="flex" flexDirection="column" alignItems="center" px={3} py={2}>
+                        <Avatar sx={{ bgcolor: 'primary.main', mb: 1 }}>
+                            {instance.getActiveAccount().name ? instance.getActiveAccount().name.charAt(0).toUpperCase() : <AccountCircle />}
+                        </Avatar>
+                        <Typography variant="subtitle1" fontWeight={600} align="center">
+                            {instance.getActiveAccount().name}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary" align="center">
+                            {instance.getActiveAccount().username}
+                        </Typography>
+                    </Box>
+                )}
+                <Divider sx={{ my: 1 }} />
+                <MenuItem onClick={() => handleLogout("popup")} key="logoutPopup" sx={{ justifyContent: 'center', fontWeight: 500 }}>
+                    Cerrar sesión
+                </MenuItem>
             </Menu>
-            <AccountPicker open={accountSelectorOpen} onClose={handleClose} />
-        </div>
+        </Box>
     )
 };
